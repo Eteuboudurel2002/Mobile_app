9f39c360197fbf396cb0b7573637af4e51f54a18
Durel63 <eteuboudurel@gmail.com>
2024-02-14 08:51:35 +0100

    server express, creation de la bd

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose()
+const cors = require('cors');
 
 const dbname = 'malaria.db'
 
+//create express app
 const app = express();
 
-let db = new sqlite3.Database(dbname, sqlite3.OPEN_READWRITE ,err => {
+//Server port
+var PORT = 8000 
+
+// create database
+let db = new sqlite3.Database(dbname,err => {
     if(err)
         throw err
     console.log('Database stated on malaria.db')
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
 
-app.listen(8000, () => {
+// Start server
+app.listen(PORT, () => {
     console.log('port is listening');
 });
 
+// Root endpoint
+app.get('/', (req, res) => {
+    res.send('Hello world');
+    res.json({"message":"Ok"});
+})
 
-
+//close the database
 db.close();
